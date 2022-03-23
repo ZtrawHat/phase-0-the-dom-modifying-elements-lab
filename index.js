19e2698b2bf92fc620e2f4aa69e75049b88adc81
Justin <justin.i7394@gmail.com>
2022-03-23 19:40:55 -0400

    commit tree 5c201c0c24cd902f01243cd3e56c1cc6731f0422

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Write your code here! 
+const main = document.getElementById("main"); 
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory"); 
+
+newHeader.textContent = "Justin is the champion"
+
+
+
+
